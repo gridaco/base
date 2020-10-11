5812f1fb6ec86fce336e07ec1810c777a29a2ced
softmarshmallow <woojoo@softmarshmallow.com>
2020-10-12 01:06:54 +0900

    added microservices template with nestjs

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # service
-*PRIVATE* service server
+> public services (all services except auth and billing) msa repository
+
+
+
+
+
+
+
+## micros
+
+- [resource-hosting](./resource-hosting)
+- application hosting
+- auth (read the development token generation docs)
+
+
+
+
+
+## Frameworks
+
+nestjs - [learn more](https://docs.nestjs.com/microservices/basics)
+
---
+++ resource-hosting/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+  });
+  app.listen(() => console.log('Microservice is listening'));
+}
+bootstrap();
