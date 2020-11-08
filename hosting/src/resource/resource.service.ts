e3a3227873bdc7c08614e385e736e87f8a6ada2c
softmarshmallow <woojoo@softmarshmallow.com>
2020-11-08 20:19:52 +0900

    updated hosting to return permanent url

--- hosting/src/resource/resource.service.ts
+++ hosting/src/resource/resource.service.ts
@@ -2,12 +2,21 @@ import { Injectable } from '@nestjs/common';
 import S3 from "aws-sdk/clients/s3"
 import { FileHostingResult } from "@bridged.xyz/client-sdk/dist/hosting/types"
 import { nanoid } from 'nanoid';
+const REGION = "us-west-1"
 const s3 = new S3({
-    region: "us-west-1"
+    region: REGION
 })
 
 const FILE_HOSTING_BUKET = 'resource-hosting';
 const SITE_HOSTING_BUKET = 'site-hosting';
+
+function buildS3ResourceUrl(props: {
+    region: string, buket: string, key: string
+}) {
+    // e.g. - https://seamus.s3.eu-west-1.amazonaws.com/dogs/setter.png
+    return `https://${props.buket}.s3.${props.region}.amazonaws.com/${props.key}`
+}
+
 
 
 @Injectable()
@@ -25,10 +34,17 @@ export class ResourceService {
 
         // Uploading files to the bucket
         await s3.upload(params).promise()
-        var url = s3.getSignedUrl('getObject', {
-            Bucket: FILE_HOSTING_BUKET,
-            Key: key,
-        });
+        const url = buildS3ResourceUrl({
+            buket: FILE_HOSTING_BUKET,
+            region: REGION,
+            key: key
+        })
+
+        // this is a signed url, which is valid for short time.
+        // var url = s3.getSignedUrl('getObject', {
+        //     Bucket: FILE_HOSTING_BUKET,
+        //     Key: key,
+        // });
 
         return <FileHostingResult>{
             url: url,
