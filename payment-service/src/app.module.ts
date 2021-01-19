36a3418c240f8febc0690874aee9eb546289546c
GwonHeeJun <developer_gwon@naver.com>
2021-01-20 02:55:36 +0900

    chore(setting) : Omissioned code upload

--- payment-service/src/app.module.ts
+++ payment-service/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { SubcriptionModule } from './subcription/subcription.module';
+import { PaymentModule } from './payment/payment.module';
 @Module({
-  imports: [],
+  imports: [SubcriptionModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
