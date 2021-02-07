506e1197d0b1e14c3c3d3a676783de22bd52bf84
softmarshmallow <woojoo@softmarshmallow.com>
2021-02-08 02:02:01 +0900

    updated scripts, using serverless finch

--- cors-service/scripts/copy-web-build.sh
+++
@@ -1,1 +0,0 @@
-cp -a web/build/. .build/public/
--- cors-service/src/app.ts
+++ cors-service/src/app.ts
@@ -24,11 +24,9 @@ const cors_proxy = corsProxy.createServer({
 
 
 
-app.use(express.static('public'))
-
 function serveDemo(req, res){
   // https://github.com/FidelLimited/serverless-plugin-optimize/issues/32
-  const file = path.resolve(process.env.LAMBDA_TASK_ROOT as string, '_optimize', process.env.AWS_LAMBDA_FUNCTION_NAME as string, 'public', 'index.html')
+  const file = path.resolve(process.env.LAMBDA_TASK_ROOT as string, '_optimize', process.env.AWS_LAMBDA_FUNCTION_NAME as string, 'web/build', 'index.html')
   res.sendFile(file);
 }
 
