6939a836b14705ebda751a52169d2169f68cd00d
softmarshmallow <woojoo@softmarshmallow.com>
2021-02-11 04:41:48 +0900

    fuck

--- cors-service/src/index.ts
+++ cors-service/src/index.ts
@@ -3,7 +3,30 @@ import { APIGatewayProxyHandler } from "aws-lambda";
 
 import { app } from "./app";
 
-const server = serverlessExpress.createServer(app);
+const binaryMimeTypes = [
+  '*/*'
+
+  // https://github.com/vendia/serverless-express/blob/master/examples/basic-starter/lambda.js
+  // 'application/javascript',
+  // 'application/json',
+  // 'application/octet-stream',
+  // 'application/xml',
+  // 'font/eot',
+  // 'font/opentype',
+  // 'font/otf',
+  // 'image/jpeg',
+  // 'image/png',
+  // 'image/svg+xml',
+  // 'text/comma-separated-values',
+  // 'text/css',
+  // 'text/html',
+  // 'text/javascript',
+  // 'text/plain',
+  // 'text/text',
+  // 'text/xml'
+]
+
+const server = serverlessExpress.createServer(app, null, binaryMimeTypes)
 
 export const handler: APIGatewayProxyHandler = (event, context) => {
   serverlessExpress.proxy(server, event, context);
