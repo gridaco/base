7e4c5ffaf92bd065aa2e3e3fe9c775097df1cf8b
softmarshmallow <woojoo@softmarshmallow.com>
2022-05-12 08:35:24 +0900

    deploy

--- posts-service/app.ts
+++ posts-service/app.ts
@@ -4,13 +4,26 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import router from "./routes";
 
+function logErrors(err, req, res, next) {
+  console.error(err.stack);
+  next(err);
+}
+
 const app = express();
 
 app.use(cors());
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
 app.use(useragent.express());
 
 app.use(bodyParser.json());
+
+app.use(logErrors);
 
 app.use(router);
 
